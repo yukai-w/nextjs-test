68b0cff6341e0c2dcc07bd4b507c86079ef99f99
yukai-w <yukai.w@outlook.com>
2019-11-27 15:24:40 +0800

    learn

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -14,6 +14,22 @@ const PostLink = ({ post }) => (
     <Link href="/p/[id]" as={`/p/${post.id}`}>
       <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
@@ -34,20 +50,6 @@ export default function Blog() {
 
         ul {
           padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
         }
       `}</style>
     </Layout>
